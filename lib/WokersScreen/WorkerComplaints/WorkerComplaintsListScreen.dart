c6fcd83c397514ea2f34fc1fcc3443bf3a63a5bc
2002-rokkam <kiran20.rokkam@gmail.com>
2025-01-22 00:25:25 +0530

    location update

--- lib/WokersScreen/WorkerComplaints/WorkerComplaintsListScreen.dart
+++ lib/WokersScreen/WorkerComplaints/WorkerComplaintsListScreen.dart
@@ -9,6 +9,7 @@ import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WorkerComplaintsListScreen extends StatelessWidget {
   final DateTime date;
@@ -224,11 +225,7 @@ class _ComplaintCardState extends State<ComplaintCard> {
     }
 
     Dio dio = Dio();
-
-    // Generate a unique timestamp
     String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
-
-    // Create a unique filename with timestamp
     String uniqueFilename = 'solved_complaint_image_$timestamp.jpg';
 
     FormData formData = FormData.fromMap({
@@ -300,14 +297,145 @@ class _ComplaintCardState extends State<ComplaintCard> {
     }
   }
 
+  void _showFullScreenImage(
+      String imageUrl, double dirlatitude, double dirlongitude) async {
+    // Get the time from the complaint (you can format it as needed)
+    final createdAt = DateTime.parse(widget.complaint['created_at']).toLocal();
+    String time = '${createdAt.hour}:${createdAt.minute}:${createdAt.second}';
+
+    // Format the latitude and longitude with 6 decimal places
+    String location =
+        'Lat: ${dirlatitude.toStringAsFixed(6)}, Long: ${dirlongitude.toStringAsFixed(6)}';
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: Stack(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Image with interactive zoom
+                  InteractiveViewer(
+                    panEnabled: true,
+                    minScale: 0.5,
+                    maxScale: 4.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Time overlay below the image
+                  Container(
+                    width: 370,
+                    height: 45,
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.86),
+                                              borderRadius: BorderRadius.circular(16),
+
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.23),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          time,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontFamily: 'Nunito Sans',
+                            fontWeight: FontWeight.w700,
+                            height: 1.43,
+                            letterSpacing: 0.14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Location (Latitude & Longitude) below the time
+                  Container(
+                    width: 370,
+                    height: 45,
+                    padding: const EdgeInsets.all(2),
+                    
+                    decoration: BoxDecoration(
+                     color: Colors.white.withOpacity(0.86),
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.23),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          location,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontFamily: 'Nunito Sans',
+                            fontWeight: FontWeight.w600,
+                            height: 1.43,
+                            letterSpacing: 0.14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _launchURL(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(widget.complaint);
     final images = widget.complaint['photos'];
     final status = widget.complaint['status'];
     final createdAt = DateTime.parse(widget.complaint['created_at']).toLocal();
     final caption = widget.complaint['caption'];
     final resolvedPhoto = widget.complaint['resolved_photo'];
+    final dirlatitude = widget.complaint['photos'][0]['latitude'];
 
+    final dirlongitude = widget.complaint['photos'][0]['longitude'];
+    String time = '${createdAt.hour}:${createdAt.minute}:${createdAt.second}';
     return Container(
       margin: EdgeInsets.only(bottom: 16.0),
       width: 370,
@@ -337,14 +465,21 @@ class _ComplaintCardState extends State<ComplaintCard> {
                     enlargeCenterPage: false,
                   ),
                   items: images.map<Widget>((image) {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Container(
-                        width: 370,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage('${image['image']}'),
-                            fit: BoxFit.cover,
+                    return GestureDetector(
+                      onTap: () {
+                        // Open image in full-screen view when tapped
+                        _showFullScreenImage(
+                            image['image'], dirlatitude, dirlongitude);
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Container(
+                          width: 370,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage('${image['image']}'),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
@@ -410,7 +545,7 @@ class _ComplaintCardState extends State<ComplaintCard> {
                       ),
                       SizedBox(width: 8),
                       Container(
-                        width: 78,
+                        width: 138,
                         height: 26,
                         padding: const EdgeInsets.symmetric(
                             horizontal: 10, vertical: 5),
@@ -422,7 +557,7 @@ class _ComplaintCardState extends State<ComplaintCard> {
                         ),
                         child: Center(
                           child: Text(
-                            '${createdAt.day}/${createdAt.month}/${createdAt.year}',
+                            '$time,${createdAt.day}/${createdAt.month}/${createdAt.year}',
                             style: TextStyle(
                               color: Color(0xFF252525),
                               fontSize: 12,
@@ -437,6 +572,7 @@ class _ComplaintCardState extends State<ComplaintCard> {
             ),
           ),
           SizedBox(height: 8),
+
           // Location and Caption
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 12.0),
@@ -458,6 +594,21 @@ class _ComplaintCardState extends State<ComplaintCard> {
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
+                    // Location Redirect Icon
+                    IconButton(
+                      icon: Icon(Icons.directions, color: Colors.blue),
+                      onPressed: () {
+                        if (dirlatitude != null && dirlongitude != null) {
+                          final url = Uri.parse(
+                              'https://www.google.com/maps?q=$_latitude,$_longitude');
+                          _launchURL(url);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Location not available'),
+                          ));
+                        }
+                      },
+                    ),
                   ],
                 ),
                 SizedBox(height: 8),
@@ -471,6 +622,7 @@ class _ComplaintCardState extends State<ComplaintCard> {
               ],
             ),
           ),
+
           SizedBox(height: 16),
           // Conditional Button
           Padding(
